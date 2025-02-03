49c66c724796157f88270c096e0aab6b6a4e8bd6
tincho <tinchoabbate@users.noreply.github.com>
2025-02-03 11:36:55 +0000

    explain storage layout better

--- test/DelegateContractV4.t.sol
+++ test/DelegateContractV4.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import {DelegateContractV3} from "../src/DelegateContractV3.sol";
 import {DelegateContractV4} from "../src/DelegateContractV4.sol";
+import "forge-std/console.sol";
 
 contract DelegateContractV4Test is Test {
     address deployer = makeAddr("deployer");
@@ -35,11 +36,48 @@ contract DelegateContractV4Test is Test {
         vm.signAndAttachDelegation(address(delegateContractV3), alice.privateKey);
         DelegateContractV3(payable(alice.addr)).initialize(guardians, v, r, s);
 
+        /**
+         * The storage layout of DelegateContractV3 is as follows:
+         * 
+         * forge inspect DelegateContractV3 storageLayout
+            ╭-----------+--------------------------+------+--------+-------+-----------------------------------------------╮
+            | Name      | Type                     | Slot | Offset | Bytes | Contract                                      |
+            +==============================================================================================================+
+            | _status   | uint256                  | 0    | 0      | 32    | src/DelegateContractV3.sol:DelegateContractV3 |
+            |-----------+--------------------------+------+--------+-------+-----------------------------------------------|
+            | init      | bool                     | 1    | 0      | 1     | src/DelegateContractV3.sol:DelegateContractV3 |
+            |-----------+--------------------------+------+--------+-------+-----------------------------------------------|
+            | guardians | mapping(address => bool) | 2    | 0      | 32    | src/DelegateContractV3.sol:DelegateContractV3 |
+            ╰-----------+--------------------------+------+--------+-------+-----------------------------------------------╯
+         */
+
         // Alice moves to V4
         vm.signAndAttachDelegation(address(delegateContractV4), alice.privateKey);
         address(0).call("");
 
-        // The account now is paused due to storage collision in V4
+        /**
+         * The storage layout of DelegateContractV4 is as follows:
+
+         * forge inspect DelegateContractV4 storageLayout
+            ╭-----------+--------------------------+------+--------+-------+-----------------------------------------------╮
+            | Name      | Type                     | Slot | Offset | Bytes | Contract                                      |
+            +==============================================================================================================+
+            | _status   | uint256                  | 0    | 0      | 32    | src/DelegateContractV4.sol:DelegateContractV4 |
+            |-----------+--------------------------+------+--------+-------+-----------------------------------------------|
+            | paused    | bool                     | 1    | 0      | 1     | src/DelegateContractV4.sol:DelegateContractV4 |
+            |-----------+--------------------------+------+--------+-------+-----------------------------------------------|
+            | guardians | mapping(address => bool) | 2    | 0      | 32    | src/DelegateContractV4.sol:DelegateContractV4 |
+            ╰-----------+--------------------------+------+--------+-------+-----------------------------------------------╯
+         */
+        
+        // Slot 0 continues to be ReentrancyGuard::_status
+        assertEq(vm.load(alice.addr, 0), bytes32(0));
+
+        // Now slot 1 is DelegateContractV4::paused instead of DelegateContractV3::init
+        // but the value remains set to 1 from the previous version.
+        assertEq(vm.load(alice.addr, bytes32(uint256(1))), bytes32(uint256(1)));
+
+        // That means the account now is paused due to storage collision
         assertTrue(DelegateContractV4(payable(alice.addr)).paused());
     }
 }
